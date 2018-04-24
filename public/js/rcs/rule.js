e2881c847ac6931966713033f33cf8ed0bacbf95
AiJiangnan <904629998@qq.com>
2018-04-24 14:10:09 +0800

    [add] 添加规则配置

--- public/js/rcs/rule.js
+++ public/js/rcs/rule.js
@@ -1,5 +1,17 @@
 layui.use('table', () => {
     const [$, t, f] = [layui.jquery, layui.table, layui.form];
+    let field = {};
+
+    $.get('/risk/enum', d => {
+        d.field.map((e, i) => {
+            field[e.value] = e.detail;
+        });
+    });
+
+    const getField = str => {
+        if (!str) return '-';
+        return field[str]
+    };
 
     t.render({
         id: 'rule',
@@ -9,42 +21,52 @@ layui.use('table', () => {
         url: '/risk/rule',
         cols: [[
             {type: 'numbers', title: '序号'},
-            {field: 'username', title: '用户名', align: 'center', width: 120},
-            {field: 'realname', title: '姓名', align: 'center', width: 120},
-            {field: 'gender', title: '性别', align: 'center', width: 80, templet: '#gender'},
-            {field: 'phone', title: '手机号码', align: 'center', width: 120},
-            {field: 'state', title: '状态', align: 'center', width: 80, sort: true, templet: '#state'},
+            {field: 'productName', title: '产品名称', align: 'center', width: 100},
+            {field: 'ruleName', title: '规则名称', align: 'center', width: 140},
+            {field: 'type', title: '类型', align: 'center', width: 80, templet: '#type'},
+            {field: 'whetherBlacklist', title: '是否加入黑名单', align: 'center', width: 140, templet: d => d.whetherBlacklist ? '是' : '否'},
+            {field: 'detail', title: '详情', align: 'center', width: 100},
+            {field: 'ruleList', title: '规则列表', align: 'center', width: 100},
+            {field: 'logical', title: '逻辑关系', align: 'center', width: 100},
+            {field: 'field', title: '字段', align: 'center', width: 120, templet: d => getField(d.field)},
+            {field: 'determine', title: '比较符', align: 'center', width: 120},
+            {field: 'param', title: '比较参数', align: 'center', width: 100},
+            {field: 'closed', title: '状态', align: 'center', width: 80, templet: '#closed'},
             {title: '操作', width: 300, align: 'center', toolbar: '#tool'}
         ]]
     });
 
     t.on('tool(rule)', o => {
         let [e, d] = [o.event, o.data];
-        if (e === 'allot') {
+        if (e === 'rule') {
+            if (d.ruleName) sessionStorage.setItem('ruleNames', d.ruleList);
             layer.open({
-                title: '分配角色',
+                title: '选择规则',
                 type: 2,
-                content: '/system/rulerole.html',
-                area: ['300px', '400px'],
+                content: '/rcs/deal/rulelist.html',
+                area: ['600px', '450px'],
                 btn: ['确认', '取消'],
-                success: (l, i) => {
-                    let f = layer.getChildFrame('form', i);
-                    f.attr('data-id', d.id);
-                },
                 yes: (i, l) => {
                     let f = layer.getChildFrame('form', i);
-                    let roleIds = [];
-                    f.find(':checked').map((i, e) => roleIds.push($(e).val()));
-                    if (roleIds.length < 1) {
-                        layer.msg('分配角色不能为空！', constants.LOCK);
+                    const ruleName = f.find(':checked');
+                    if (ruleName.length < 1) {
+                        layer.msg('没有选择任何规则！', constants.LOCK);
                         return;
                     }
-                    $.post('/user/role', {userId: d.id, roleIds: JSON.stringify(roleIds)}, data => {
+                    let ruleNames = '';
+                    ruleName.map((i, e) => {
+                        ruleNames += $(e).val();
+                        if (i !== ruleName.length - 1) ruleNames += ',';
+                    });
+                    $.post('/risk/rule', {id: d.id, ruleList: ruleNames}, data => {
                         if (data.code === 0) {
                             layer.msg(data.data, constants.SUCCESS);
-                            layer.close(i);
+                            o.update({ruleList: ruleNames});
+                        } else {
+                            layer.msg(data.msg, constants.ERROR);
                         }
-                    }).fail(() => layer.msg('服务器错误！'), constants.FAIL);
+                        layer.close(i);
+                    }).fail(() => layer.msg('服务器错误！', constants.FAIL));
                 },
                 btn2: (i, l) => {
                     layer.close(i);
@@ -53,66 +75,51 @@ layui.use('table', () => {
         }
         if (e === 'edit') {
             layer.open({
-                title: '修改管理员',
+                title: '修改规则',
                 type: 2,
-                content: ['/system/editrule.html', 'no'],
-                area: ['300px', '300px'],
+                content: ['/rcs/deal/rule.html', 'no'],
+                area: ['400px', '460px'],
                 success: (l, i) => {
-                    let f = layer.getChildFrame('form', i);
-                    for (let k in d) {
-                        if (k === 'gender') continue;
-                        f.find("input[name='" + k + "']").val(d[k]);
-                    }
-                    f.find("input[name='gender'][value=" + d.gender + "]").prop('checked', true);
-                    f.find("input[name='statebox']").attr('checked', d.state);
-                },
-                end: () => {
-                    if (sessionStorage.getItem('user')) {
-                        let u = JSON.parse(sessionStorage.getItem('user'));
-                        u.state = (u.state === 'true');
-                        o.update(u);
-                        sessionStorage.removeItem('user');
-                    }
+                    sessionStorage.setItem('rule', JSON.stringify(d));
                 }
             });
         }
         if (e === 'onoff') {
-            let s = d.state;
-            const m = '<span style="color:red;">' + (s ? '停用' : '启用') + '</span>';
-            layer.confirm(`你确定要${m}该管理员！`, constants.WARM, i => {
-                $.post('/user', {id: d.id, state: !s}, data => {
+            let s = d.closed;
+            const m = '<span style="color:red;">' + (!s ? '停用' : '启用') + '</span>';
+            layer.confirm(`你确定要${m}该规则！`, constants.WARM, i => {
+                $.post('/risk/rule', {id: d.id, closed: !s}, data => {
                     if (data.code === 0) {
-                        layer.msg(`管理员${m}成功！`, constants.SUCCESS);
-                        o.update({state: !s});
+                        layer.msg(`规则${m}成功！`, constants.SUCCESS);
+                        o.update({closed: !s});
                         return;
                     }
-                    layer.msg(`管理员${m}失败！`, constants.ERROR);
+                    layer.msg(`规则${m}失败！`, constants.ERROR);
                 }).fail(() => layer.msg('服务器错误！'), constants.FAIL);
                 layer.close(i);
             });
         }
         if (e === 'del') {
-            layer.confirm('你确定要' + r`删除` + '该管理员吗？', constants.FAIL, i => {
-                $.post('/user/' + d.id, data => {
+            layer.confirm('你确定要' + r`删除` + '该规则吗？', constants.FAIL, i => {
+                $.post('/risk/rule/' + d.id, data => {
                     if (data.code === 0) {
                         layer.msg(data.data, constants.SUCCESS);
                         o.del();
                         return;
                     }
-                    layer.msg('管理员删除失败！', constants.ERROR);
+                    layer.msg('规则删除失败！', constants.ERROR);
                 }).fail(() => layer.msg('服务器错误！'), constants.FAIL);
                 layer.close(i);
             });
         }
     });
 
-
     $('#add').click(() => {
         layer.open({
-            title: '新建管理员',
+            title: '新建规则',
             type: 2,
-            content: ['/system/newrule.html', 'no'],
-            area: ['300px', '380px']
+            content: ['/rcs/deal/rule.html', 'no'],
+            area: ['400px', '460px']
         });
     });
 
