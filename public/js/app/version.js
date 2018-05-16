19940f7f3ae3d0ed7dada4f39c5be324d60ee194
liyapeng <liyapeng@xyb100.com>
2018-05-16 18:10:22 +0800

    添加版本管理功能

--- public/js/app/version.js
+++ public/js/app/version.js
@@ -30,23 +30,39 @@ layui.use(['table', 'laydate'], () => {
             {field: 'versionName', title: '版本名称', align: 'center', width: 110},
             {field: 'type', title: '系统版本', align: 'center', width: 110,templet: d => typeConstants.STATUS[d.type]},
             {field: 'versionNum', title: '版本号', align: 'center', width: 110},
-            {field: 'url', title: '版本地址', align: 'center', width: 280},
             {field: 'versionDate', title: '创建时间', align: 'center', width: 160,templet: d => dateTimeFormat(d.versionDate)},
-            {field: 'versionStatus', title: '版本更新状态', align: 'center', width: 160,templet: d => driveConstants.STATUS[d.type]},
-            {field: 'versionDesc', title: '版本说明', align: 'center', width: 230},
-            {title: '操作', width: 180, align: 'center', toolbar: '#tool'}
+            {field: 'versionStatus', title: '版本更新状态', align: 'center', width: 130,templet: d => driveConstants.STATUS[d.type]},
+            {field: 'versionDesc', title: '版本说明', align: 'center', width: 180},
+            {field: 'url', title: '版本地址', align: 'center', width: 200},
+            {title: '操作', width: 160, align: 'center', toolbar: '#tool'}
         ]]
     });
 
     t.on('tool(version)', o => {
         let [e, d] = [o.event, o.data];
-        check(d);
-        if (e === 'deviceInfo') {
-           
+        if (e === 'del') {
+            layer.confirm('确定要' + r`删除` + '该APP版本？', constants.FAIL, i => {
+                $.post('/app/version/del/'+d.id, data => {
+                    if (data.code === 0) {
+                        layer.msg(data.data, constants.SUCCESS);
+                        o.del();
+                        return;
+                    }
+                    layer.msg('版本删除失败！', constants.ERROR);
+                }).fail(() => layer.msg('服务器错误！'), constants.FAIL);
+                layer.close(i);
+            });
         }
     });
     
-    
+    $('#add').click(() => {
+        layer.open({
+            title: '新建版本',
+            type: 2,
+            content: ['/app/addversion.html', 'no'],
+            area: ['400px', '380px']
+        });
+    });
     f.on('submit(submit)', d => {
         d.field.page = 1;
         t.reload('version', {where: d.field});
